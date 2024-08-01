388785a930dcc10f250676ac7e22ca1442550308
atropos <sv7n@pm.me>
2024-08-01 07:56:44 +0100

    Added annotations for homepage usages for the app to create homer pages.

--- internal/controller/get_homepage_plugin.go
+++ internal/controller/get_homepage_plugin.go
@@ -4,6 +4,8 @@ import (
 	atroxyzv1alpha1 "github.com/atropos112/atrok.git/api/v1alpha1"
 )
 
+// TODO: Remove the gethomepage.dev stuff once moved away from gethomepage.
+
 func GetHomePageAnnotations(ingress_annotations map[string]string, ab *atroxyzv1alpha1.AppBundle) map[string]string {
 	annotations := make(map[string]string)
 
@@ -12,32 +14,41 @@ func GetHomePageAnnotations(ingress_annotations map[string]string, ab *atroxyzv1
 	}
 
 	annotations["gethomepage.dev/enabled"] = "true"
+	annotations["atro.xyz/homepage/enabled"] = "true"
 
 	if ab.Spec.Homepage.Description != nil {
+		annotations["atro.xyz/homepage/description"] = *ab.Spec.Homepage.Description
 		annotations["gethomepage.dev/description"] = *ab.Spec.Homepage.Description
 	}
 
 	if ab.Spec.Homepage.Instance != nil {
+		annotations["atro.xyz/homepage/instance"] = *ab.Spec.Homepage.Instance
 		annotations["gethomepage.dev/instance"] = *ab.Spec.Homepage.Instance
 	}
 
 	if ab.Spec.Homepage.Group != nil {
+		annotations["atro.xyz/homepage/group"] = *ab.Spec.Homepage.Group
 		annotations["gethomepage.dev/group"] = *ab.Spec.Homepage.Group
 	} else {
+		annotations["atro.xyz/homepage/group"] = "Other"
 		annotations["gethomepage.dev/group"] = "Other"
 	}
 
 	if ab.Spec.Homepage.Href != nil {
+		annotations["atro.xyz/homepage/href"] = *ab.Spec.Homepage.Href
 		annotations["gethomepage.dev/href"] = *ab.Spec.Homepage.Href
 	}
 
 	if ab.Spec.Homepage.Icon != nil {
+		annotations["atro.xyz/homepage/icon"] = *ab.Spec.Homepage.Icon
 		annotations["gethomepage.dev/icon"] = *ab.Spec.Homepage.Icon
 	}
 
 	if ab.Spec.Homepage.Name != nil {
+		annotations["atro.xyz/homepage/name"] = *ab.Spec.Homepage.Name
 		annotations["gethomepage.dev/name"] = *ab.Spec.Homepage.Name
 	} else {
+		annotations["atro.xyz/homepage/name"] = ab.Name
 		annotations["gethomepage.dev/name"] = ab.Name
 	}
 
