f6bc08ae4bcef65b8a70412932e68c5e446f25ab
atropos <sv7n@pm.me>
2024-09-11 22:29:02 +0100

    User auth set if homepage set

--- internal/controller/get_homepage_plugin.go
+++ internal/controller/get_homepage_plugin.go
@@ -21,6 +21,11 @@ func GetHomePageAnnotations(annotations map[string]string, ab *atroxyzv1alpha1.A
 
 	if ab.Spec.Homepage.Groups != nil {
 		newAnnotations["atro.xyz/homepage.groups"] = *ab.Spec.Homepage.Groups
+
+		// If we want user to see the page in their homepage, we want the user to have access also, assuming its not set already.
+		if _, ok := newAnnotations["atro.xyz/auth.group"]; !ok {
+			newAnnotations["atro.xyz/auth.group"] = *ab.Spec.Homepage.Groups
+		}
 	}
 
 	if ab.Spec.Homepage.Section != nil {
